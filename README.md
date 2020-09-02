eae146c78a41c2a7a009b1fc2a9b6c01cd309c05
Samuel <sampaioxsamuel@gmail.com>
2020-09-02 18:26:56 -0300

    Fix filme não encontrado; mudei link da api e outras pequenas melhorias na interface

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # imdb-js
-Aplicação feita com javascript vanilla, que busca por um filme e mostra sua capa, data de lançamento e uma breve sinopse
+Aplicação feita com javascript vanilla, que busca por um filme e mostra sua capa, data de lançamento e uma sinopse
--- src/js/app.js
+++ src/js/app.js
@@ -7,25 +7,28 @@ const nextBtn = document.querySelector("[data-next]");
 const previousBtn = document.querySelector("[data-previous]");
 
 const backgroundUrl = (url) =>
-	`https://image.tmdb.org/t/p/w1920_and_h800_multi_faces${url}`;
+	`https://image.tmdb.org/t/p/w1920_and_h1080_multi_faces${url}`;
 const posterUrl = (url) =>
 	`https://image.tmdb.org/t/p/w600_and_h900_bestv2${url}`;
 const theMovieDB = (name, API_KEY) =>
-	fetch(
-		`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${name}`);
+	API_KEY
+		? fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${name}&language=pt-BR&en-US`)
+		: alert("You need an API KEY");
 
 let mvNumber = 0;
+const API_KEY = ""; // GET YOUR API KEY IN themoviedb.org;
+
+previousBtn.classList.add("disable");
 
 function nextMovie() {
-	
-		if(mvNumber > 18){
-			nextBtn.classList.add("disable");
-		}else{
-			resultContainer.innerHTML = "";
-			mvNumber++;
-			previousBtn.classList.remove("disable");
-			showMovie(mvNumber);
-		}
+	if (mvNumber > 18) {
+		nextBtn.classList.add("disable");
+	} else {
+		resultContainer.innerHTML = "";
+		mvNumber++;
+		showMovie(mvNumber);
+		previousBtn.classList.remove("disable");
+	}
 }
 
 function previousMovie() {
@@ -33,9 +36,9 @@ function previousMovie() {
 		mvNumber--;
 		resultContainer.innerHTML = "";
 		showMovie(mvNumber);
-		nextBtn.classList.remove("disable");
 	} else {
 		previousBtn.classList.add("disable");
+		alert("Você está no primeiro filme encontrado!");
 	}
 }
 
@@ -51,22 +54,21 @@ function showMovie(movieNumber) {
 	theMovieDB(movieName, API_KEY)
 		.then((response) => response.json())
 		.then((movieFound) => {
-			if(typeof(movieFound.results[mvNumber]) === 'undefined'){
-				nextBtn.classList.add("disable");
+			
+			if(typeof movieFound.results[mvNumber + 1] === 'undefined'){
+				nextBtn.classList.add('disable')
 			}
+			
+			const backdrop = movieFound.results[movieNumber].backdrop_path;
 			const movieTitle = movieFound.results[movieNumber].title;
 			const movieOverview = movieFound.results[movieNumber].overview;
-			const moviePoster =
-				movieFound.results[movieNumber].poster_path || null;
-			const releaseDate = JSON.stringify(
-				movieFound.results[movieNumber].release_date
-			)
-				.split("-")[0]
-				.replace('"', " ");
-			const backdrop = movieFound.results[movieNumber].backdrop_path;
-
+			const moviePoster = movieFound.results[movieNumber].poster_path;
+			const movieDate = movieFound.results[movieNumber].release_date
+			
+			const finalYearRelease = (typeof movieDate === 'undefined') ? 'Data de Lançamento Desconhecida' : movieDate.split('-')[0]
+		
 			title.innerText = movieTitle;
-			year.innerText = releaseDate;
+			year.innerText = finalYearRelease;
 			overview.innerText = movieOverview;
 
 			poster.setAttribute(
@@ -75,6 +77,7 @@ function showMovie(movieNumber) {
 					? posterUrl(moviePoster)
 					: "https://dummyimage.com/600x900/000/fff.png&text=No+Cover"
 			);
+
 			poster.classList.add("movie__cover");
 			title.classList.add("movie__title");
 			year.classList.add("movie__year");
@@ -86,25 +89,31 @@ function showMovie(movieNumber) {
 			movieDiv.append(overview);
 
 			backgroundImg.style.background = `url(${backgroundUrl(backdrop)})`;
+			backgroundImg.style.backgroundRepeat = "no-repeat";
 			resultContainer.append(movieDiv);
-		});
+		})
+		.catch(() => {
+			alert('Filme não encontrado, tente novamente!')
+			location.reload()
+		})
 }
 
 searchInput.addEventListener("keydown", () => {
 	if (event.key === "Enter") {
 		if (searchInput.value === "") {
-			alert("input empty");
-			searchInput.classList.add('erro')
+			alert("You need");
+			searchInput.classList.add("erro");
 			setTimeout(() => {
-				searchInput.classList.remove('erro')
-			}, 2000)
+				searchInput.classList.remove("erro");
+			}, 2000);
 		} else {
 			searchContainer.classList.add("hidden");
 			showMovie(mvNumber);
+
 			nextBtn.style.display = "initial";
 			previousBtn.style.display = "initial";
 
-			nextBtn.addEventListener("click", nextMovie)
+			nextBtn.addEventListener("click", nextMovie);
 			previousBtn.addEventListener("click", previousMovie);
 		}
 	}
